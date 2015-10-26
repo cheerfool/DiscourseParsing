8834bfa2252f5842c1d2f87fb198fff0940a1907
Kailang Jiang <jiangkl@cs.ubc.ca>
2015-10-26 16:35:04 -0700

    Initial commit

---
+++ src/granularity/CountSpan.java
@@ -0,0 +1,12 @@
+package granularity;
+
+public class CountSpan {
+	
+	public static final String dir = "D:/Data/RST-DT/TRAINING";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
